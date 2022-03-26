c6d9161d3cd4762de2e19559fcac43a70c131fc4
Alexis CHEMEL <chemel@users.noreply.github.com>
2022-03-26 16:12:54 +0100

    Create Feed entity

---
+++ src/Entity/Feed.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FeedRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FeedRepository::class)]
+class Feed
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $url;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $title;
+
+    #[ORM\Column(type: 'boolean')]
+    private $enabled;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $fetchedAt;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getFetchedAt(): ?\DateTimeImmutable
+    {
+        return $this->fetchedAt;
+    }
+
+    public function setFetchedAt(?\DateTimeImmutable $fetchedAt): self
+    {
+        $this->fetchedAt = $fetchedAt;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
